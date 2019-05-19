ed68a56a065601f9ee97df46fbad37f554e98fa6
Ullapool <thomaslang82@bluewin.ch>
2019-05-19 13:45:06 +0200

    cleared unused variables

--- soundbox.h
+++ soundbox.h
@@ -27,17 +27,17 @@ public:
     QVector<QString> ballonStriked;
     QVector<QString> opponentHit;
     QString soundtrack;
-    QString shooting;
-    QString shooting_2;
-    QString shooting_3;
-    QString opponentStriked;
-    QString opponentStriked_2;
-    QString opponentStriked_3;
-    QString strike;
-    QString strike_2;
-    QString shootOpponent;
-    QString shootOpponent_2;
-    QString goalCheering;
+//    QString shooting;
+//    QString shooting_2;
+//    QString shooting_3;
+//    QString opponentStriked;
+//    QString opponentStriked_2;
+//    QString opponentStriked_3;
+//    QString strike;
+//    QString strike_2;
+//    QString shootOpponent;
+//    QString shootOpponent_2;
+      QString goalCheering;
 
     static Soundbox *getInstance();
     void playSoundEffect(Sound sound);
